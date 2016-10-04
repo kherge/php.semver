7a07e74d44d7a6d401cb0f2bda792832ee2cacec
Kevin Herrera <kevin@herrera.io>
2016-10-04 09:00:05 -0700

    Creating a value object for a semantic version number.

---
+++ src/KHerGe/Version/Version.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace KHerGe\Version;
+
+/**
+ * Manages the information for a semantic version number as a value object.
+ *
+ * @author Kevin Herrera <kherrera@ebsco.com>
+ */
+class Version implements VersionInterface
+{
+    /**
+     * The build metadata.
+     *
+     * @var string[]
+     */
+    private $build;
+
+    /**
+     * The major version number.
+     *
+     * @var integer
+     */
+    private $major;
+
+    /**
+     * The minor version number.
+     *
+     * @var integer
+     */
+    private $minor;
+
+    /**
+     * The patch version number.
+     *
+     * @var integer
+     */
+    private $patch;
+
+    /**
+     * The pre-release metadata.
+     *
+     * @var string[]
+     */
+    private $preRelease;
+
+    /**
+     * Initializes the new semantic version number.
+     *
+     * @param integer  $major      The major version number.
+     * @param integer  $minor      The minor version number.
+     * @param integer  $patch      The patch version number.
+     * @param string[] $preRelease The pre-release metadata.
+     * @param string[] $build      The build metadata.
+     */
+    public function __construct(
+        int $major,
+        int $minor,
+        int $patch,
+        array $preRelease,
+        array $build
+    ) {
+        $this->build = $build;
+        $this->major = $major;
+        $this->minor = $minor;
+        $this->patch = $patch;
+        $this->preRelease = $preRelease;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBuild() : array
+    {
+        return $this->build;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMajor() : int
+    {
+        return $this->major;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMinor() : int
+    {
+        return $this->minor;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPatch() : int
+    {
+        return $this->patch;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPreRelease() : array
+    {
+        return $this->preRelease;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isStable() : bool
+    {
+        return (0 < $this->major) && empty($this->preRelease);
+    }
+}
---
+++ tests/KHerGe/Version/VersionTest.php
@@ -0,0 +1,187 @@
+<?php declare(strict_types=1);
+
+namespace Test\KHerGe\Version;
+
+use KHerGe\Version\Version;
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * Verifies that the semantic version number functions as intended.
+ *
+ * @author Kevin Herrera <kevin@herrera.io>
+ *
+ * @coversDefaultClass \KHerGe\Version\Version
+ *
+ * @covers ::__construct
+ */
+class VersionTest extends TestCase
+{
+    /**
+     * The build metadata.
+     *
+     * @var string[]
+     */
+    private $build = ['x', 'y', 'z'];
+
+    /**
+     * The major version number.
+     *
+     * @var integer
+     */
+    private $major = 1;
+
+    /**
+     * The minor version number.
+     *
+     * @var integer
+     */
+    private $minor = 2;
+
+    /**
+     * The patch version number.
+     *
+     * @var integer
+     */
+    private $patch = 3;
+
+    /**
+     * The pre-release metadata.
+     *
+     * @var string[]
+     */
+    private $preRelease = ['a', 'b', 'c'];
+
+    /**
+     * The semantic version number.
+     *
+     * @var Version
+     */
+    private $version;
+
+    /**
+     * Returns the version numbers to check for stability.
+     *
+     * @return array[] The test case arguments.
+     */
+    public function getStabilityVersions()
+    {
+        return [
+
+            // unstable: 0.1.2
+            [new Version(0, 1, 2, [], []), false],
+
+            // unstable: 1.2.3-a
+            [new Version(1, 2, 3, ['a'], []), false],
+
+            // stable: 1.2.3
+            [new Version(1, 2, 3, [], []), true]
+
+        ];
+    }
+
+    /**
+     * Verify that the build metadata can be retrieved.
+     *
+     * @covers ::getBuild
+     */
+    public function testRetrieveTheBuildMetadata()
+    {
+        self::assertEquals(
+            $this->build,
+            $this->version->getBuild(),
+            'The build metadata was not returned.'
+        );
+    }
+
+    /**
+     * Verify that the major version number can be retrieved.
+     *
+     * @covers ::getMajor
+     */
+    public function testRetrieveTheMajorVersionNumber()
+    {
+        self::assertEquals(
+            $this->major,
+            $this->version->getMajor(),
+            'The major version number was not returned.'
+        );
+    }
+
+    /**
+     * Verify that the minor version number can be retrieved.
+     *
+     * @covers ::getMinor
+     */
+    public function testRetrieveTheMinorVersionNumber()
+    {
+        self::assertEquals(
+            $this->minor,
+            $this->version->getMinor(),
+            'The minor version number was not returned.'
+        );
+    }
+
+    /**
+     * Verify that the patch version number can be retrieved.
+     *
+     * @covers ::getPatch
+     */
+    public function testRetrieveThePatchVersionNumber()
+    {
+        self::assertEquals(
+            $this->patch,
+            $this->version->getPatch(),
+            'The patch version number was not returned.'
+        );
+    }
+
+    /**
+     * Verify that the pre-release metadata can be retrieved.
+     *
+     * @covers ::getPreRelease
+     */
+    public function testRetrieveThePreReleaseMetadata()
+    {
+        self::assertEquals(
+            $this->preRelease,
+            $this->version->getPreRelease(),
+            'The pre-release metadata was not returned.'
+        );
+    }
+
+    /**
+     * Verify that the stableness can be checked.
+     *
+     * @param Version $version The version to check.
+     * @param boolean $stable  Is it stable?
+     *
+     * @covers ::isStable
+     *
+     * @dataProvider getStabilityVersions
+     */
+    public function testCheckIfVersionIsStable(Version $version, bool $stable)
+    {
+        self::assertSame(
+            $stable,
+            $version->isStable(),
+            sprintf(
+                'The number was unexpectedly %s.',
+                $stable ? 'unstable' : 'stable'
+            )
+        );
+    }
+
+    /**
+     * Creates a new semantic version number.
+     */
+    protected function setUp()
+    {
+        $this->version = new Version(
+            $this->major,
+            $this->minor,
+            $this->patch,
+            $this->preRelease,
+            $this->build
+        );
+    }
+}
